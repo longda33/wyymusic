ca9e879e80eee98e825c8cbf15f0ea5eb5c5c491
longda33 <45527745+longda33@users.noreply.github.com>
2020-03-04 15:15:21 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,236 @@
+<!DOCTYPE html>
+<html lang="en">
+ <head> 
+  <meta charset="utf-8" /> 
+  <title>唯爱音乐接口开放平台 - 网页悬浮音乐播放器_WordPress|Emlog|Z-Blog|Typecho音乐插件_明月浩空播放器破解版</title> 
+  <meta name="viewport" content="width=device-width, initial-scale=1" /> 
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" /> 
+  <meta name="description" content="Vi Network是一款简约的HTML悬浮音乐播放器,支持多网站音乐解析,明月浩空播放器破解版" /> 
+  <meta name="keywords" content="Vi Network,明月浩空破解版,wordpress音乐插件,emlog音乐插件,typecho音乐插件,z-blog音乐插件,TML5音乐播放器,唯爱网页播放器" /> 
+  <link href="/PlayerFile/Adminla/bootstrap.min.css" rel="stylesheet" type="text/css" media="all" /> 
+  <link href="/PlayerFile/Adminla/Adminla.css" rel="stylesheet" /> 
+  <link rel="stylesheet" href="/PlayerFile/Adminla/animate.css" /> 
+  <link rel="stylesheet" href="/PlayerFile/Adminla/owl.carousel.css" /> 
+  <link rel="stylesheet" href="/PlayerFile/Adminla/owl.theme.css" /> 
+  <link rel="stylesheet" href="/PlayerFile/Adminla/ionicons.min.css" /> 
+  <link href="/PlayerFile/Adminla/style.css" rel="stylesheet" type="text/css" media="all" /> 
+ </head> 
+ <body> 
+  <div class="wrapper"> 
+   <nav class="navbar navbar-expand-md navbar-light bg-light fixed-top"> 
+    <div class="container container-s"> 
+     <a class="navbar-brand" href="">Vi Network</a> 
+     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"> <span class="navbar-toggler-icon"></span> </button> 
+     <div class="collapse navbar-collapse" id="navbarSupportedContent"> 
+      <ul class="navbar-nav ml-auto navbar-right"> 
+       <li class="nav-item"><a class="btn-cta nav-link js-scroll-trigger" href="/Storagen">立即使用</a></li> 
+       <li class="nav-item"><a class="btn-cta nav-link js-scroll-trigger" href="https://<?php echo $_SERVER['HTTP_HOST'];?>/FM">FM音乐</a></li> 
+       <li class="nav-item"><a class="btn-cta nav-link js-scroll-trigger" target="_blank" href="http://www.vizyw.com/">唯爱官网</a></li> 
+       <li class="nav-item"><a class="btn-cta nav-link js-scroll-trigger" target="_blank" href="http://wpa.qq.com/msgrd?v=3&amp;uin=1452938190&amp;site=qq&amp;menu=yes">联系站长</a></li> 
+      </ul> 
+     </div> 
+    </div> 
+   </nav> 
+   <div id="main" class="main"> 
+    <div class="home"> 
+     <div class="container"> 
+      <div class="row text-center"> 
+       <div class="col-md-12"> 
+        <div class="hero-img wow fadeIn"> 
+         <img class="img-fluid" src="/Storages/big.png" alt="Home" /> 
+        </div> 
+       </div> 
+       <div class="col-md-12"> 
+        <div class="hero-content wow fadeIn"> 
+         <h1>HTML悬浮音乐播放器 - Vi Network</h1> 
+         <p>免费、稳定、安全、快速、人性化的HTML音乐播放器</p> 
+         <a class="btn-action js-scroll-trigger" href="/Storagen">开始使用</a> 
+         <a class="btn-action js-scroll-trigger" href="/FM">听音乐</a> 
+        </div> 
+       </div> 
+      </div> 
+     </div> 
+    </div> 
+    <div id="products" class="features wow fadeInDown"> 
+     <div class="container-m"> 
+      <div class="row text-center"> 
+       <div class="col-md-12"> 
+        <div class="features-intro"> 
+         <h2>播放器的功能</h2> 
+         <p>目前播放器的功能并不是很完善、（Vi Network）正在努力开发中</p> 
+        </div> 
+       </div> 
+       <div class="col-md-6 col-lg-4"> 
+        <div class="feature-list"> 
+         <div class="card-icon"> 
+          <div class="card-img"> 
+           <img src="/Storages/a1.png" width="35" alt="Feature" /> 
+          </div> 
+         </div> 
+         <div class="card-text"> 
+          <h3>多平台解析</h3> 
+          <p>目前Vi Network支持网易云音乐、QQ音乐、酷狗音乐三种网站音乐的解析,只需要获取到ID就可以播放歌曲了</p> 
+         </div> 
+        </div> 
+       </div> 
+       <div class="col-md-6 col-lg-4"> 
+        <div class="feature-list"> 
+         <div class="card-icon"> 
+          <div class="card-img"> 
+           <img src="/Storages/a2.png" width="35" alt="Feature" /> 
+          </div> 
+         </div> 
+         <div class="card-text"> 
+          <h3>自匹配歌词</h3> 
+          <p>Vi Network播放器可以自动获取歌曲的歌词、用户可以选择歌词的开关、歌词根据歌曲播放进度自动滚动</p> 
+         </div> 
+        </div> 
+       </div> 
+       <div class="col-md-6 col-lg-4"> 
+        <div class="feature-list"> 
+         <div class="card-icon"> 
+          <div class="card-img"> 
+           <img src="/Storages/a3.png" width="35" alt="Feature" /> 
+          </div> 
+         </div> 
+         <div class="card-text"> 
+          <h3>个性化平台</h3> 
+          <p>Vi Network平台通过Key方式绑定用户个人的播放器和歌单、不会与其他用户产生冲突、达成多人使用的目的</p> 
+         </div> 
+        </div> 
+       </div> 
+      </div> 
+     </div> 
+    </div> 
+    <div id="features" class="flex-split"> 
+     <div class="container-s"> 
+      <div class="flex-intro align-center wow fadeIn"> 
+       <h2>效果预览</h2> 
+       <p>我不是人民币无需任何人都喜欢我、努力做好自己、走自己的路让别人说去吧</p> 
+      </div> 
+      <div class="flex-inner align-center"> 
+       <div class="f-image wow"> 
+        <img class="img-fluid" src="/Storages/feature.png" alt="Feature" /> 
+       </div> 
+       <div class="f-text"> 
+        <div class="left-content"> 
+         <h2>Vi Network</h2> 
+         <p>页面简洁好看、使用操作简单、适用于所有网站程序、如需使用点击导航使用文档即可查看详情、源站已使用CDN加速不会造成网站访问变慢</p> 
+        </div> 
+       </div> 
+      </div> 
+     </div> 
+    </div> 
+    <div class="yd-stats wow fadeIn"> 
+     <div class="container-s"> 
+      <div class="row text-center"> 
+       <div class="col-sm-12"> 
+        <div class="intro"> 
+         <h2>唯爱网络平台真实数据</h2> 
+        </div> 
+       </div> 
+       <div class="col-sm-4"> 
+        <div class="counter-up"> 
+         <h3><span class="counter">2000+</span></h3> 
+         <div class="counter-text"> 
+          <h2>用户数量</h2> 
+         </div> 
+        </div> 
+       </div> 
+       <div class="col-sm-4"> 
+        <div class="counter-up"> 
+         <h3><span class="counter">5000+</span></h3> 
+         <div class="counter-text"> 
+          <h2>播放器数量</h2> 
+         </div> 
+        </div> 
+       </div> 
+       <div class="col-sm-4"> 
+        <div class="counter-up"> 
+         <h3><span class="counter">10000+</span></h3> 
+         <div class="counter-text"> 
+          <h2>解析次数</h2> 
+         </div> 
+        </div> 
+       </div> 
+       <div class="col-md-12"> 
+        <div class="yd-link"> 
+         <a href="/"></a> 
+        </div> 
+       </div> 
+      </div> 
+     </div> 
+    </div> 
+    <div class="ar-ft-single wow fadeIn"> 
+     <div class="container-m"> 
+      <div class="ar-feature"> 
+       <div class="ar-list"> 
+        <ul> 
+         <li> 
+          <div class="ar-icon"> 
+           <img src="/Storages/f1.png" width="30" alt="Icon" /> 
+          </div> 
+          <div class="ar-text"> 
+           <h3>快速访问</h3> 
+           <p>Vi Network播放器资源采用CDN加速、让资源加载的快些再快些吧</p> 
+          </div> </li> 
+         <li> 
+          <div class="ar-icon"> 
+           <img src="/Storages/f4.png" width="30" alt="Icon" /> 
+          </div> 
+          <div class="ar-text"> 
+           <h3>永久免费</h3> 
+           <p>本人在这里保证,播放器永久免费,如果你觉得播放器不错的话,可以自愿赞助我哦</p> 
+          </div> </li> 
+         <li> 
+          <div class="ar-icon"> 
+           <img src="/Storages/f11.png" width="30" alt="Icon" /> 
+          </div> 
+          <div class="ar-text"> 
+           <h3>意见反馈</h3> 
+           <p>如果你对本平台有哪些意见和不满可以发送邮件到1452938190@qq.com,我会听取意见进行改进</p> 
+          </div> </li> 
+        </ul> 
+       </div> 
+       <div class="ar-image"> 
+        <img class="ar-img img-fluid" src="/Storages/feature-2.png" alt="Hero Image" /> 
+       </div> 
+      </div> 
+     </div> 
+    </div> 
+    <div class="footer-sm"> 
+     <div class="container"> 
+      <div class="row"> 
+       <div class="col-md-4"> 
+        <a class="footer-logo" href="">Vi Network</a> 
+       </div> 
+       <div class="col-md-4"> 
+        <h6>CopyRight &copy; <a href="">Vi Network</a> 2014-2019</h6> 
+       </div> 
+       <div class="col-md-4"> 
+        <ul> 
+         <li><a href="/Storagen">立即使用</a></li> 
+         <li><a href="http://wpa.qq.com/msgrd?v=3&amp;uin=1452938190&amp;site=qq&amp;menu=yes">联系站长</a></li> 
+    
+         <li><a href="https://<?php echo $_SERVER['HTTP_HOST'];?>/FM">FM音乐</a></li> 
+        </ul> 
+       </div> 
+      </div> 
+     </div> 
+    </div> 
+    <!--Scroll To Top--> 
+    <div id="back-top" class="bk-top"> 
+     <div class="bk-top-txt"> 
+      <a class="back-to-top js-scroll-trigger" href="#main"><i class="fa fa-chevron-up"></i></a> 
+     </div> 
+    </div> 
+    <!--Scroll To Top Ends--> 
+   </div> 
+  </div> 
+  <!--播放器代码开始--> 
+  <script type="text/javascript" src="https://libs.baidu.com/jquery/2.1.4/jquery.min.js"></script> 
+  <script>auto="open";random="open";name="Vi Network";geci="colse";user="117504348";welcome="open";tips="欢迎光临唯爱FM";</script> 
+  <script type="text/javascript" src="https://<?php echo $_SERVER['HTTP_HOST'];?>/wyplayer/music.js"></script> 
+  <!--播放器代码结束-->  
+ </body>
+</html>
